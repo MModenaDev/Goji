d89a5a58bdb3adc200362a8e95b0d32c3c3ca947
Guilherme Tadanori Mori <gtm.mori@gmail.com>
2019-10-17 18:18:35 -0300

    API google maps implemented

--- models/stores.js
+++ models/stores.js
@@ -2,12 +2,13 @@ const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
 const storesSchema = new Schema({
-  name: String,
   adress: String,
+  location: { type: { type: String }, coordinates: [Number] } // coordinates [longitude, latitude]
 },
 {
   timestamps: true,
 })
+storesSchema.index({ location: '2dsphere' });
 
-const Stores = mongoose.model("Leads", storesSchema);
+const Stores = mongoose.model("Stores", storesSchema);
 module.exports = Stores;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,9 @@
 // Packages and Models require 
 const express = require('express');
 const router  = express.Router();
+const axios = require('axios')
 const Leads = require(`../models/leads`);
-
-
-
+const Store = require(`../models/stores`);
 
 // =====================================================================================================================================
 // Landing Page
@@ -43,7 +42,43 @@ router.get('/dashboard', (req, res, next) => {
     )
 })
 
+// =====================================================================================================================================
+// Google API - Latitude/Longitude
 
+router.get('/adress', (req, res, next) => {
+  const { adress } = req.query
+  const adressValue = adress.trim().replace(/ /g, '+');
+  const adressAPI = axios.create({baseURL: `https://maps.googleapis.com/maps/api/geocode/json?address=${adressValue}&key=${process.env.GOOGLE_KEY}`})
+  adressAPI
+    .get()
+    .then(adressInfo => {
+      const { lat, lng } = adressInfo.data.results[0].geometry.location;
+
+      const location = {
+        type: 'Point',
+        coordinates: [lng, lat]
+        };
+
+      const newStore = new Store ({
+        adress,
+        location: location,
+      })
+      
+      newStore.save()
+        .then(res.redirect('/adress'))
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+})
+
+// =====================================================================================================================================
+// Sending stores to Front-End
+
+router.get('/stores', (req, res, next) => {
+  Store.find()
+    .then(store => {res.json(store)})
+    .catch(err => console.log(err))
+})
   
   
 
